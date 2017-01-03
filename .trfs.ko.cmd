b6582ce3e1478dff38acba27950d07bccfa77ab9
Jay <mengyang@cs.stonybrook.edu>
2017-01-04 02:20:51 +0800

    remove old version files

--- .trfs.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/usr/src/hw2-mengyang/fs/trfs/trfs.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /usr/src/hw2-mengyang/fs/trfs/trfs.ko /usr/src/hw2-mengyang/fs/trfs/trfs.o /usr/src/hw2-mengyang/fs/trfs/trfs.mod.o
